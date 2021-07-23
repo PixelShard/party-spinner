f5f38f586ad3b02927e6a8abc964bbb7efc26f44
Lukáš Mahr <mahr@able.cz>
2021-07-24 00:29:09 +0200

    fix randoming spin id on resest. Smaller font.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
 import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Item, NgxWheelComponent, TextAlignment, TextOrientation } from "ngx-wheel";
 
+export interface FullItem extends Item {
+  textFontSize?: number;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  @ViewChild(NgxWheelComponent, { static: false })
+  @ViewChild(NgxWheelComponent, {static: false})
   wheel: NgxWheelComponent | undefined;
 
   title = 'party-spinner';
   // ngx-wheel setup
   public width = 600;
   public height = 600;
-  public items: Item[] = [
-    {text: 'Pivo', fillStyle: '#b07547', id: 0},
-    {text: 'Paddleboard', fillStyle: '#70d4c0', id: 1},
-    {text: 'Limo', fillStyle: '#b7ad50', id: 2},
-    {text: 'Koupačka', fillStyle: '#4579b8', id: 3},
-    {text: 'Jídlo', fillStyle: '#887a74', id: 4},
-    {text: 'Houpačka', fillStyle: '#eda0f1', id: 4},
-    {text: 'Lodě', fillStyle: '#e2f1a0', id: 4},
+  public items: FullItem[] = [
+    {text: 'Pivo', fillStyle: '#b07547', id: 0, textFontSize: 16},
+    {text: 'Paddleboard', fillStyle: '#70d4c0', id: 1, textFontSize: 16},
+    {text: 'Limo', fillStyle: '#b7ad50', id: 2, textFontSize: 16},
+    {text: 'Koupačka', fillStyle: '#4579b8', id: 3, textFontSize: 16},
+    {text: 'Jídlo', fillStyle: '#887a74', id: 4, textFontSize: 16},
+    {text: 'Houpačka', fillStyle: '#eda0f1', id: 4, textFontSize: 16},
+    {text: 'Lodě', fillStyle: '#e2f1a0', id: 4, textFontSize: 16},
   ];
   public textOrientation: TextOrientation = TextOrientation.HORIZONTAL;
   public textAlignment: TextAlignment = TextAlignment.CENTER;
@@ -36,22 +41,28 @@ export class AppComponent implements OnInit {
 
   setInnerWidth() {
     this.innerWidth = window.innerWidth;
-    console.log(this.innerWidth);
     this.width = innerWidth < this.width ? innerWidth : 600;
     this.height = this.width;
   }
 
   onSpin() {
-    this.reset();
-    if (this.wheel) {
-      this.wheel.spin();
-    }
+    this.reset().then(res => {
+        this.idToLandOn = this.seed[Math.floor(Math.random() * this.seed.length)];
+        if (this.wheel) {
+          this.wheel.spin();
+        }
+      }
+    );
   }
 
-  private reset() {
-    if (this.wheel) {
-      this.wheel.reset()
-    }
+  private reset(): Promise<boolean> {
+    return new Promise((resolve) => {
+      if (this.wheel) {
+        this.wheel.reset()
+        resolve(true);
+      }
+      resolve(false);
+    });
   }
 
   public before() {
